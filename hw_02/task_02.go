2984367fdd1a394ea337109dc65a8eeebc7f10ba
cybittheir <kibitzer.vl@hotmail.com>
2024-02-21 22:07:07 +1000

    task 2 done

--- hw_02/task_02.go
+++ hw_02/task_02.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("ReHi")
+	fn := "list.txt"
+	f, err := os.Open(fn)
+	if err != nil {
+		fmt.Println("File not opened: ", err)
+		return
+	}
+	defer f.Close()
 
+	s, err := os.Stat(fn)
+	if err != nil {
+		fmt.Println("File error: ", err)
+		return
+	}
+
+	sz := s.Size()
+
+	buf := make([]byte, sz)
+	if _, err := io.ReadFull(f, buf); err != nil {
+		fmt.Println("Cannot read file: ", err)
+		return
+	}
+
+	fmt.Printf("File %s, size=%d bytes:\n", fn, sz)
+	fmt.Printf("%s\n", buf)
+	fmt.Println("Bye.")
 }
