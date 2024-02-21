c33f190f0bbfe47b795378900da227f0557bb91a
cybittheir <kibitzer.vl@hotmail.com>
2024-02-21 21:45:39 +1000

    task 1 done

---
+++ hw_02/task_01.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	fn := "list.txt"
+	f, err := os.Create(fn)
+	if err != nil {
+		fmt.Println("File not open", err)
+		return
+	}
+	fmt.Println("Введите предложение:")
+	reader := bufio.NewReader(os.Stdin)
+	var n int
+	var tx string
+	for {
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		tm := time.Now()
+		t := strings.TrimSpace(text)
+
+		if strings.ToLower(t) == "exit" {
+			break
+		} else if t == "" {
+			continue
+		}
+		n++
+		tt := fmt.Sprintf("%d. %s %s\n", n, tm.Format("02/01/2006 15:04:05"), t)
+		fmt.Println(tt)
+		tx += tt
+	}
+	defer f.Close()
+	f.WriteString(tx)
+	fmt.Println("Bye.")
+}
