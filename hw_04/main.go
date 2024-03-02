8d61642b88b9766a1a34b4713b7a8f28c1cd2c7f
cybittheir <kibitzer.vl@hotmail.com>
2024-03-02 17:04:33 +1000

    fixed all known mistakes

--- hw_04/main.go
+++ hw_04/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"slices"
@@ -37,17 +38,29 @@ type User struct {
 
 func (u *User) ToString() string {
 
-	bp := "User %s with id%d and age of %d y.o. has "
+	bp := "User <b>%s</b> with <u>id%d</u> and age of %d y.o. has "
 
 	fUrl := fmt.Sprintf("<a href=/friendship/id%d>", u.Id)
 
 	if len(u.Friends) == 0 {
-		return fmt.Sprintf(bp+"no %sfriends</a>.\n", u.Name, u.Id, u.Age, fUrl)
+		return fmt.Sprintf(bp+"no %sfriends</a>", u.Name, u.Id, u.Age, fUrl)
 	} else if len(u.Friends) == 1 {
-		return fmt.Sprintf(bp+"only %sfriend %s</a>.\n", u.Name, u.Id, u.Age, fUrl, strings.Join(u.Friends, ", "))
+		return fmt.Sprintf(bp+"only %sfriend</a> ", u.Name, u.Id, u.Age, fUrl)
 	} else {
-		return fmt.Sprintf(bp+"%d %sfriends</a>: %s.\n", u.Name, u.Id, u.Age, len(u.Friends), fUrl, strings.Join(u.Friends, ", "))
+		return fmt.Sprintf(bp+"%d %sfriends</a>: ", u.Name, u.Id, u.Age, len(u.Friends), fUrl)
 	}
+}
+
+func (b *base) AllFriends(u *User) string {
+	res := make([]string, 0)
+
+	for _, v := range b.storage[u.Id].Friends {
+		if vi, err := strconv.Atoi(v); err == nil {
+			res = append(res, b.storage[vi].Name+"<i>(id"+fmt.Sprint(vi)+"; age "+fmt.Sprint(b.storage[vi].Age)+")</i>")
+		}
+	}
+
+	return strings.Join(res, ", ")
 }
 
 func (u *User) Numbered() string {
@@ -61,12 +74,12 @@ func (u *User) Checked(c bool) string {
 
 	bp := ""
 	if c {
-		bp = "<input type=checkbox name=id_%d id=id_%d checked><label for=id_%d>%s (%d)</label>"
+		bp = "%d.<input type=checkbox name=id_%d id=id_%d checked><label for=id_%d>%s (%d)</label>"
 
 	} else {
-		bp = "<input type=checkbox name=id_%d id=id_%d><label for=id_%d>%s (%d)</label>"
+		bp = "%d.<input type=checkbox name=id_%d id=id_%d><label for=id_%d>%s (%d)</label>"
 	}
-	return fmt.Sprintf(bp+"\n", u.Id, u.Id, u.Id, u.Name, u.Age)
+	return fmt.Sprintf(bp+"\n", u.Id, u.Id, u.Id, u.Id, u.Name, u.Age)
 }
 
 func nl2br(in string) string {
@@ -180,11 +193,11 @@ func (b *base) Create(w http.ResponseWriter, r *http.Request) {
 		var u User
 		//TODO: detect Content-type: application/json
 		if http.DetectContentType(content) != "" {
-			fmt.Println(http.DetectContentType(content))
+			log.Println(http.DetectContentType(content))
 		}
 
 		if st, err := u.QueryParse(content); err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 		} else {
 			if err := json.Unmarshal(st, &u); err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
@@ -218,7 +231,7 @@ func (b *base) GetAll(w http.ResponseWriter, r *http.Request) {
 
 		response := ""
 		for _, user := range b.storage {
-			response += user.ToString()
+			response += user.ToString() + b.AllFriends(user) + ".\n"
 		}
 		w.WriteHeader(http.StatusOK)
 		menu := ""
@@ -327,13 +340,13 @@ func (b *base) ChangeAge(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 			return
 		}
 		defer r.Body.Close()
 
 		if st, err := QueryParse(content); err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 		} else {
 			menu = home + ageForm + list
 			if uId, err := strconv.Atoi(st["id"]); err != nil {
@@ -424,9 +437,25 @@ func (b *base) SetRelations(w http.ResponseWriter, r *http.Request) {
 
 					}
 				}
-				//				oldFriends := b.storage[q].Friends
+				oldFriends := b.storage[q].Friends
+				if len(oldFriends) > 0 {
+					for _, va := range oldFriends {
+						if v, err := strconv.Atoi(va); err != nil {
+							log.Println("Err. convert:", err)
+						} else {
+							if i, err := contains(b.storage[v].Friends, q); err != nil {
+								log.Println("Err. nothing to do:", err)
+							} else if _, err := contains(friends, v); err != nil {
+								b.storage[v].Friends[i] = b.storage[v].Friends[len(b.storage[v].Friends)-1]
+								b.storage[v].Friends[len(b.storage[v].Friends)-1] = ""
+								b.storage[v].Friends = b.storage[v].Friends[:len(b.storage[v].Friends)-1]
+							}
+						}
+					}
+				}
+
 				b.storage[q].Friends = friends
-				//TODO: remove deleted friends
+
 				w.Write([]byte(useTemplate("Relationship set", b.storage[q].Name+" friendship updated", menu)))
 			}
 			return
@@ -438,7 +467,7 @@ func (b *base) RemoveFriend(uid int) {
 	for i, v := range b.storage {
 		if i != uid {
 			if ind, err := contains(v.Friends, uid); err != nil {
-				fmt.Println("Err. nothing to do:", err)
+				log.Println("Err. nothing to do:", err)
 			} else {
 				v.Friends[ind] = v.Friends[len(v.Friends)-1]
 				v.Friends[len(v.Friends)-1] = ""
